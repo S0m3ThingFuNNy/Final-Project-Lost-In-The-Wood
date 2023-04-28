4c94583913efaf484c93e2e77910b0f31d73ea5c
S0m3ThingFuNNy <78758224+S0m3ThingFuNNy@users.noreply.github.com>
2023-04-28 04:07:26 +0000

    project

--- Main.java
+++ Main.java
@@ -6,8 +6,8 @@ import java.util.Random;
 public class Main {
   
   private static int wood = 0;
-  
 
+  
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input;
